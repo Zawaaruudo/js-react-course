79b78fb551d64ffe8868d6c8b0e2ee046366af13
Zawaaruudo <124351887+Zawaaruudo@users.noreply.github.com>
2025-07-04 13:54:50 +0300

    used axios to get data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,13 +1,12 @@
 import { Header } from '../components/Header';
 import { products } from '../../starting-code/data/products'
 import CheckmarkIcon from '../assets/images/icons/checkmark.png'
+import axios from 'axios';
 import './HomePage.css';
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
+  axios.get('http://localhost:3000/api/products')
     .then((response) => {
-      response.json().then((data) => {
-        console.log(data);
-      })
+      console.log(response.data);
     })
   return (
     <>
